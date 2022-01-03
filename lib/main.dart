58d179bdec719d62279ee4455ecd4f6362c7eed6
Bhagyesh <bhagyesh.imscit17@gmail.com>
2022-01-03 11:34:08 +0530

    Explaining Routes,initial Routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app2/home_page.dart';
+import 'package:flutter_app2/pages/login_page.dart';
+import 'pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,8 +10,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     int days = 30;
+    bringVegetables(rupees: 50);
     return MaterialApp(
-      home:HomePAge(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(brightness: Brightness.light),
+      initialRoute: "/home",
+      routes: {
+        "/":(context) => LoginPage(),
+        "/home":(context) => HomePAge(),
+        "/login":(context) => LoginPage(),
+      },
     );
+  }
+
+  bringVegetables({int rupees = 100}){
+
   }
 }
