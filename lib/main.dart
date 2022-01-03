7aa26677e53869ce891cc4b8e5742df46526132e
Bhagyesh <bhagyesh.imscit17@gmail.com>
2022-01-03 10:50:55 +0530

    AppBar Basic Complete

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app2/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    int days = 30;
+    return MaterialApp(
+      home:HomePAge(),
+    );
+  }
+}
